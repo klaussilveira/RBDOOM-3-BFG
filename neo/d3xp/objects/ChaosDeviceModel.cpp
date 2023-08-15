b5198925ba1c4b2aeeb6b11ddc3f7981fa99015a
LegendaryGuard <rootuser999@gmail.com>
2023-08-15 18:02:01 +0200

    Add HeXen: Edge Of Chaos source code - objects and projectiles directories (part 4)

---
+++ neo/d3xp/objects/ChaosDeviceModel.cpp
@@ -0,0 +1,17 @@
+
+#include "precompiled.h"
+#pragma hdrstop
+
+#include "../Game_local.h"
+#include "ChaosDeviceModel.h"
+
+CLASS_DECLARATION( idEntity, idEntity_ChaosDeviceModel)
+
+END_CLASS
+
+void idEntity_ChaosDeviceModel::Spawn() { }
+
+void idEntity_ChaosDeviceModel::Think()
+{
+	idEntity::Think();
+}
